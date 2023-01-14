54050af8a1862e0b7c118245b932d9d5a9d3f456
Alex Steel <alexsteel@Emilys-MacBook-Pro-2.local>
2023-01-13 16:42:45 -0800

    first commit# Please enter the commit message for your changes. Lines starting

---
+++ LightlyGrilled/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LightlyGrilled
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static void Test() { }
+    }
+}

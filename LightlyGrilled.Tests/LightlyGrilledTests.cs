3560c6b5857b70fddf38e0b791472bef311af468
Alex Steel <alexsteel@Emilys-MacBook-Pro-2.local>
2023-01-26 08:27:53 -0800

    adding tests for two sum

--- LightlyGrilled.Tests/LightlyGrilledTests.cs
+++ LightlyGrilled.Tests/LightlyGrilledTests.cs
@@ -7,9 +7,18 @@ namespace LightlyGrilled.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TwoSumTests()
         {
-            Program.Test();
+            TwoSumTestBase(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 });
+            TwoSumTestBase(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 });
+            TwoSumTestBase(new int[] { 3, 3 }, 6, new int[] { 0, 1 });
+        }
+        public void TwoSumTestBase(int[] nums, int target, int[] expectedResult)
+        {
+            int[] actual = LightlyGrilledQuestions.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            Assert.NotNull(actual);
+            Assert.Equal(2, actual.Length);
+            Assert.Equal(expectedResult, actual);
         }
     }
 }
--- LightlyGrilled/LightlyGrilledQuestions.cs
+++ LightlyGrilled/LightlyGrilledQuestions.cs
@@ -2,12 +2,37 @@
 
 namespace LightlyGrilled
 {
-    public class Program
+    public class LightlyGrilledQuestions
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
-        public static void Test() { }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            result[0] = 0;
+            result[1] = 1;
+            return result;
+        }
+
+        /* TWO SUM
+         
+        Problem:        
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+        You can return the answer in any order.
+
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+        Solution:
+
+        Time complexity: 
+         */
+         
     }
 }

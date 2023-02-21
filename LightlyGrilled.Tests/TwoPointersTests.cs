765bf2b3186311964825ffd8e6b30cfd3f43bec7
Alex Steel <saucewagon@Alexs-Mac-mini.local>
2023-02-21 08:37:56 -0800

    Starting grokking

---
+++ LightlyGrilled.Tests/TwoPointersTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace LightlyGrilled.Tests
+{
+	public class TwoPointersTests
+	{
+        [Fact]
+        public void PairWithTargetSumTest1()
+        {
+            PairWithTargetSumTestBase(new int[] { 1, 2, 3, 4, 6 }, 6, new int[] { 1 , 3});
+        }
+        [Fact]
+        public void PairWithTargetSumTest2()
+        {
+            PairWithTargetSumTestBase(new int[] { 2, 5, 9, 11 }, 11, new int[] { 0,2 });
+        }
+        [Fact]
+        public void PairWithTargetSumTest3()
+        {
+            PairWithTargetSumTestBase(new int[] { -1, 4, 45, 110 }, 44, new int[] { 0, 2 });
+        }
+        public void PairWithTargetSumTestBase(int[] nums, int target, int[] expectedResult)
+        {
+            int[] actual = TwoPointers.PairWithTargetSum(nums, target);
+            Assert.NotNull(actual);
+            Assert.Equal(2, actual.Length);
+            Assert.Equal(expectedResult, actual);
+        }
+    }
+}
+
---
+++ LightlyGrilled/TwoPointers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LightlyGrilled
+{
+	public class TwoPointers
+	{
+        public static int[] PairWithTargetSum(int[] nums, int target)
+        {
+            int endPointer = nums.Length - 1;
+            int startPointer = 0;
+            int[] pair = new int[2];
+
+            while (startPointer != endPointer)
+            {
+                int currentSum = nums[startPointer] + nums[endPointer];
+
+                if (currentSum == target)
+                {
+                    pair[0] = startPointer;
+                    pair[1] = endPointer;
+                    return pair;
+                }
+                else if (currentSum < target)
+                {
+                    startPointer++;
+                }
+                else if (currentSum > target)
+                {
+                    endPointer--;
+                }
+            }
+            return pair;
+        }
+    }
+}
+

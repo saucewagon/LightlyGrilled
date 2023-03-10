a1aaf029f1d2c02c689b3a92fafa2f7290303305
Alex Steel <saucewagon@Alexs-Mac-mini.local>
2023-03-09 20:18:17 -0800

    linked list cycle

---
+++ LightlyGrilled.Tests/FastAndSlowPointersTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xunit;
+
+namespace LightlyGrilled.Tests
+{
+	public class FastAndSlowPointersTests
+	{
+		[Fact]
+		public void LinkedListHasCyclePositiveTest()
+		{
+			ListNode head = new ListNode(1, null);
+			ListNode trav = head;
+			ListNode cycleNode = new ListNode(); 
+			int i = 2;
+			while (i <= 6)
+			{
+				trav.Next = new ListNode(i, null);
+				trav = trav.Next;
+
+				if (i == 3)
+				{
+					cycleNode = trav;
+				}
+				i++;
+
+			}
+			trav.Next = cycleNode;
+
+			PrintLinkedList(head);
+			Assert.True(FastAndSlowPointers.LinkedListHasCycle(head));
+		}
+        [Fact]
+        public void LinkedListHasCycleNegativeTest()
+        {
+            ListNode head = new ListNode(1, null);
+            ListNode trav = head;
+            int i = 2;
+            while (i <= 6)
+            {
+                trav.Next = new ListNode(i, null);
+                trav = trav.Next;
+                i++;
+            }
+
+            PrintLinkedList(head);
+            Assert.False(FastAndSlowPointers.LinkedListHasCycle(head));
+        }
+        private void PrintLinkedList(ListNode head)
+		{
+			int count = 0;
+			ListNode trav = head;
+			while (trav != null)
+			{
+				Console.Write(trav.Value + " ");
+				trav = trav.Next;
+
+				count++;
+				if (count == 10) break;
+			}
+			Console.WriteLine();
+		}
+	}
+}
+
---
+++ LightlyGrilled/FastAndSlowPointers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace LightlyGrilled
+{
+	public class ListNode
+	{
+		public int Value { get; set; }
+		public ListNode Next { get; set; }
+		public ListNode(int value, ListNode next)
+		{
+			Value = value;
+			Next = next;
+		}
+
+        public ListNode()
+        {
+        }
+    }
+	public class FastAndSlowPointers
+	{
+		public static bool LinkedListHasCycle(ListNode head)
+		{
+			if (head == null) return false;
+			ListNode fast = head.Next;
+			ListNode slow = head;
+
+			while (fast != null && fast.Next != null)
+			{
+				if (fast == slow)
+				{
+					return true;
+				}
+				slow = slow.Next;
+				fast = fast.Next.Next;
+			}
+			return false;
+		}
+	}
+}
+
